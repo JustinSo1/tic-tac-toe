58fbf223ba7e3eac404a6fe785d207e76e45d7fa
JustinSo1 <sojustin28@gmail.com>
2020-05-13 00:42:28 -0400

    Updates

---
+++ precache-manifest.174e3097e11e47162b9e5786f07a8ec2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7ee7217ec0116cd8907da2ee439585",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "aa5d42380371a1b3af59",
+    "url": "/tic-tac-toe/static/js/2.e4130cae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tic-tac-toe/static/js/2.e4130cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4822ce078c66329ba33",
+    "url": "/tic-tac-toe/static/js/main.f7658d5c.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
