2e3c93001ae57f91e99acd7e2cd2852ecb12981d
JustinSo1 <sojustin28@gmail.com>
2020-05-17 13:35:34 -0400

    Adding darkMode theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
 import Game from "./components/Game";
-const App = () => <Game />;
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import { createMuiTheme, ThemeProvider, MuiThemeProvider } from "@material-ui/core/styles";
+
+const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Game />
+    </ThemeProvider>
+  );
+};
 
 export default App;
